2ce629db2f76147049934d004bea23fa034654df
Santiago Quintana <san3091@gmail.com>
2020-05-20 12:19:53 -0500

    add http auth

---
+++ app/controllers/concerns/http_auth_concern.rb
@@ -0,0 +1,14 @@
+module HttpAuthConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :http_authenticate
+  end
+
+  def http_authenticate
+    return true unless Rails.env == 'production'
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '' && password == ''
+    end
+  end
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -11,4 +11,3 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
-
