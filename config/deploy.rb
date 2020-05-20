16f02347d8bb52e35faee40ed56cdabbb080c730
Santiago Quintana <san3091@gmail.com>
2020-05-20 09:39:20 -0500

    updated secrets and github url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.0"
 
 set :application, "catalytic-soundstream"
-set :repo_url, "git@example.com:san3091/catalytic-streaming-admin.git"
+set :repo_url, "git@github.com:san3091/catalytic-streaming-admin.git"
 
 # Deploy to user's home dir
 set :deploy_to, "/home/deploy/#{fetch :application}"
