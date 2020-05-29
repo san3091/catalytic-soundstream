cfebb7738977611e70c81786601e0df4bebf1356
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-05-28 20:17:27 -0500

    animate logo

--- app/javascript/app/Header/Header.svelte
+++ app/javascript/app/Header/Header.svelte
@@ -1,18 +1,12 @@
 <script>
   import Profile from './Profile/Profile.svelte'
-  import CatalyticSoundLogo from '../../assets/catalytic-sound-logo-(1)-WEB.png'
-  
+  import Logo from './Logo/Logo.svelte'
 </script>
 
 <div class='header'>
   <div class='header-content'>
-    <a href='https://catalyticsound.com/' 
-    target="_blank" rel="noopener noreferrer">
-      <img src={CatalyticSoundLogo} alt='Catalytic Sound Logo' >
-    </a>
-    <div class='header-right'>
-      <Profile />
-    </div>
+    <Logo />
+    <Profile />
   </div>
 </div>
 
@@ -23,7 +17,7 @@
     width: 100%;
     display: flex;
     z-index: 3;
-    background-color: var(--transparent-orange);
+    /* background-color: var(--transparent-orange); */
     /* background-color: var(--medium-grey); */
     border-bottom: 1px solid var(--orange);
   }
@@ -34,22 +28,5 @@
     align-items: center;
     justify-content: space-between;
     width: 100%;
-  }
-
-  img {
-    height: 60px;
-    margin-left: 50px;
-    position: relative;
-    top: 5px;
-  }
-
-  a:hover {
-    background-color: transparent;
-  }
-
-  .header-right {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
   }
 </style>
