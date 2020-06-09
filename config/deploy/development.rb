f188d8376f508672620f9c8813cc7d3d5f8092cb
Santiago Quintana <san3091@gmail.com>
2020-06-08 19:26:03 -0500

    Using development env to setup staging
    TODO create staging env

---
+++ config/deploy/development.rb
@@ -0,0 +1,1 @@
+server '167.71.87.188', user: 'deploy', roles: %w{app db web}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,1 @@
-server '159.89.54.144', user: 'deploy', roles: %w{app db web}
+# server '159.89.54.144', user: 'deploy', roles: %w{app db web}
