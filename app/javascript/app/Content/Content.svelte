e68c3c51b06b26d5fad71fb5c7a6e924ca3995e8
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-06-16 17:29:27 -0500

    refactor mission statement into separate component

--- app/javascript/app/Content/Content.svelte
+++ app/javascript/app/Content/Content.svelte
@@ -1,10 +1,10 @@
 <script>
   import { onMount } from 'svelte'
-  import { fade } from 'svelte/transition'
   import Search from './Search/Search.svelte'
   import Section from './Section/Section.svelte'
   import Player from './Player/Player.svelte'
-  import { playerIsOpen, modalIsOpen, user, mobileLayout, logoHover, padding} from '../stores.js'
+  import Mission from './Mission/Mission.svelte'
+  import { playerIsOpen, modalIsOpen, user, mobileLayout } from '../stores.js'
   
   let categories
   let selectedAlbum
@@ -43,22 +43,7 @@
 
 <div class='content' class:mobile={$mobileLayout}>
   <div class='music-selection' class:mobile={$mobileLayout}> 
-    {#if $logoHover}
-      <div 
-        class='mission'
-        style='--mission-padding:{$padding}px'
-        transition:fade >
-          <p>
-            The music streaming industry makes over ten billion dollars each year selling music made by more than a million artists. Despite their reliance on the creativity and labor of musicians, Spotify, Apple, Google, and other streaming services pay artists almost nothing, and treat musicians as replaceable cogs in a content-production machine.
-          </p>
-          <p>
-            The Catalytic Soundstream is our solution to this systemic imbalance: a subscription-based, artist owned and curated streaming service that pays Catalytic Sound co-op musicians 2/3 of all streaming revenue.  
-          </p>
-          <p>
-            Subscribers will enjoy a constantly shifting set of albums on the Soundstream each month: a Radio selection of thirty rotating albums chosen by Catalytic artists and staff, the continuing set of Catalytic Artist Album editions, and History Is What's Happening, a handpicked selection of records released before the new millennium. In the process they'll contribute to a new kind of music streaming service, one that provides an ongoing and equitable source of income for the musicians responsible for its creative content.
-          </p>
-        </div>
-      {/if}
+    <Mission />
     {#if categories}
       <Section
         headerText='Catalytic Radio'
@@ -146,12 +131,4 @@
     width: 100%;
   }
 
-  .mission {
-    position: relative;
-    margin: 10px var(--mission-padding) 0;
-  }
-
-  .mission p {
-    line-height: 28px;
-  }
 </style>
