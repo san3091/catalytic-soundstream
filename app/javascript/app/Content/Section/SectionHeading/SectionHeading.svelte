cc6a569a427e9b843aafe072b2d00bc16f7a4b84
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2020-10-11 15:18:46 -0500

    fix styles after extracting section header

--- app/javascript/app/Content/Section/SectionHeading/SectionHeading.svelte
+++ app/javascript/app/Content/Section/SectionHeading/SectionHeading.svelte
@@ -8,9 +8,10 @@
   export let dontMissAlbum
   export let playDontMissAlbum
   export let mousedown
+  export let highlight
 </script>
 
-<div class='section-top'>
+<div class='section-top' class:highlight>
   <div class='section-header'>
     <h2>{headerText}</h2>
     <p class='description'>{sectionDescription}</p>
@@ -34,3 +35,115 @@
     </div>
   {/if}
 </div>
+
+<style>
+h2, p {
+    margin-left: 50px;
+  }
+
+  b {
+    font-weight: 400;
+    margin-right: 10px;
+    color: var(--orange);
+  }
+
+  span {
+    color: var(--black);
+  }
+
+  h6 {
+    text-align: left;
+		white-space: unset;
+  }
+  .highlight h2 {
+    color: var(--white);
+  }
+
+  .highlight p {
+    color: var(--light-grey);
+  }
+
+  .section-top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+  }
+
+  .description {
+    margin-bottom: 30px;
+  }
+
+  .button-container {
+    padding-bottom: 40px;
+    margin-right: 20px;
+  }
+
+  .play-last-button {
+    position: relative;
+    padding: 5px 15px;
+    border: none;
+    box-sizing: border-box;
+    border: 1px solid var(--transparent-orange);
+    background-color: var(--light-grey);
+    border-radius: 0;
+    cursor: pointer;
+  }
+
+  .play-last-button::after {
+    content: '';
+    position: absolute;
+    background-color: var(--black);
+    opacity: 0;
+    top: 4px;
+    left: 4px;
+    height: 100%;
+    width: 100%;
+    z-index: -2;
+    animation: fade-in 1s 0.5s ease-in forwards;
+    border-radius: 0;
+  }
+
+  .play-last-button:hover{
+    top: -1px;
+    left: -1px;
+  }
+
+  .play-last-button:hover::after {
+    top: 5px;
+    left: 5px;
+  }
+  
+  .play-last-button.mousedown {
+     top: 0px;
+     left: 0px;
+  }
+
+ .play-last-button.mousedown::after {
+    top: 2px;
+    left: 2px;
+  }
+
+  .selected, .play-last-button.selected:hover {
+    top: -2px;
+    left: -2px;
+  }
+
+  .selected::after, .play-last-button.selected:hover::after {
+    top: 5px;
+    left: 5px;
+  }
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+      background-color: transparent;
+    }
+    50% {
+      background-color: transparent;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
