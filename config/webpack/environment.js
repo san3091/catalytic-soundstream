90fc64130ef44e7168b7cd59e40bc508aeeeafab
Santiago Quintana <san3091@gmail.com>
2022-02-12 16:58:44 -0600

    add to webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const svelte = require('./loaders/svelte')
+const webpack = require("webpack") 
 
 environment.loaders.prepend('svelte', svelte)
+environment.plugins.append("Provide", new webpack.ProvidePlugin({ 
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))  
+
 module.exports = environment
