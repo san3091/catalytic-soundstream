7d9698b252b6a84255024c3e4c935023cee49aeb
Santiago Quintana Garcia <santiago_quintana_garcia@external.mckinsey.com>
2022-02-18 09:38:12 -0600

    add last updated and fix seeds

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -5,7 +5,7 @@ puts "Created categories #{category_names.join(", ")}"
 admin = Admin.create!(name: "admin", email: Rails.application.credentials.admin_email, password: Rails.application.credentials.admin_password)
 puts 'Created sample admin'
 
-categories.map { |category| category.import_albums("lib/#{category.name}.csv", 'replace') }
+categories.map { |category| category.import_albums("lib/#{category.name}.csv") }
 puts 'Added seed albums to categories'
 
 Daddy.import_daddies('lib/grandfathered_users.csv')
